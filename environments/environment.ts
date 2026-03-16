2ec725c3f39f874c94583e05efa05b6dace49d68
mandeepBartwal <mandeep.bartwal@zouma.ai>
2026-03-16 12:29:24 +0530

    code

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://10.101.52.244:8002/api/',
+  apiUrl: 'http://localhost:8002/api/',
   // apiUrl: 'https://dev.snapsend.co:566/api/',
   // apiUrl: 'http://10.101.32.161:2008/api/',
 };
